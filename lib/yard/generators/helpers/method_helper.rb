edae25965153e7f54576cc23c99f5adb2fb44b55
Nathan Weizenbaum <nex342@gmail.com>
2009-05-23 23:23:18 -0700

    Revert "Add #format_overload to properly format an overload method signature"
    This reverts commit 529932986673beae65065e8a84d5a311516f3843.

--- lib/yard/generators/helpers/method_helper.rb
+++ lib/yard/generators/helpers/method_helper.rb
@@ -23,11 +23,6 @@ module YARD
           ""
         end
       end
-      
-      # The @overload signature can be pretty much used word for word
-      def format_overload(overload)
-        h overload.signature.gsub(/\A\s*(def\s+)?[^ \(]+/, '')
-      end
     end
   end
 end
